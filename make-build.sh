d71b024319ddb87cf6b39f0734e9e04de92fe6e1
stolen <2026438995@qq.com>
2020-09-25 11:47:37 +0800

    workspace

---
+++ make-build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+touch build.sh
+chmod +x build.sh
+
+echo #!/bin/sh
+echo "cd build" >> build.sh
+echo "cmake .." >> build.sh
+echo "cp compile_commands.json ../" >> build.sh 
+echo "cp compile"
+echo "make all" >> build.sh
+echo echo ------------------------------------ >> build.sh
+echo echo >> build.sh
+echo ./$1 >> build.sh
+echo echo >> build.sh
+echo echo ------------------------------------ >> build.sh
+
+
---
+++ make-project.sh
@@ -0,0 +1,33 @@
+
+#!/bin/sh
+
+echo Create Project $1
+echo Main: $2
+
+mkdir $1
+cd $1
+
+touch CMakeLists.txt
+
+# init cmake lists
+echo "cmake_minimum_required(VERSION 3.18.2)" >> CMakeLists.txt
+echo  >> CMakeLists.txt
+echo "project($1)" >> CMakeLists.txt
+echo  >> CMakeLists.txt
+echo "set(CMAKE_EXPORT_COMPILE_COMMANDS ON)" >> CMakeLists.txt
+echo  >> CMakeLists.txt
+echo "include_directories(include)" >> CMakeLists.txt
+echo "aux_source_directory(src sources)" >> CMakeLists.txt
+echo  >> CMakeLists.txt
+echo "add_executable($1 \${sources})" >> CMakeLists.txt
+
+mkdir src
+mkdir include
+mkdir build
+
+cp ../.main.c src/$2
+
+# init build
+../make-build.sh $1
+
+
